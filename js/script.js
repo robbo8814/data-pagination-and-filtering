b964c019035c2fd4f3205b1ffc01359b9643e6cc
Matt <matthewrobbierobinson@gmail.com>
2022-06-19 13:25:27 -0700

    displaying student data on screen

--- js/script.js
+++ js/script.js
@@ -17,6 +17,23 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+const currentStudent = document.querySelector("body ul");
+
+let currentStudentData = ''
+for (i=0; i<data.length; i++) {
+   currentStudentData += `<li class="student-item cf">
+   <div class="student-details">
+   <img class="avatar" src="${data[i].picture.medium}" alt="Profile Picture">
+   <h3>${data[i].name.first} ${data[i].name.last}</h3>
+   <span class="email">${data[i].email}</span>
+   </div>
+   <div class="joined-details">
+   <span class="date">Joined ${data[i].registered.date}</span>
+   </div>
+   </li>`
+}
+
+currentStudent.innerHTML = currentStudentData;
 
 
 
